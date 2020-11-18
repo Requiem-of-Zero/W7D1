39b6cbb9cfa7c0f995cde6bfa199341d28107625
Sam Wong <theesamwong@gmail.com>
2020-11-17 22:38:38 -0800

    99cats project

---
+++ 99cats_auth/app/models/user.rb
@@ -0,0 +1,51 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  user_name       :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+
+validates :session_token, presence: true, uniqueness: true
+after_initialize :ensure_session_token
+
+attr_reader :password
+
+  has_many :cats, class_name: "Cat", foreign_key: "user_id"
+
+  def self.find_by_credentials(user_name, password)
+    user = User.find_by(user_name: user_name)
+
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+
+end
